4c811ee45f3bef69a1349a29fd541f206aa93e58
nift4 <36458596+nift4@users.noreply.github.com>
2019-02-09 14:18:28 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup
+setup(
+# Name
+name='pigame',
+# Version
+version='1.0',
+# Desc
+description='Raspberry Pi drivers.',
+# We
+author='PiGameDrv',
+# Start Page
+url='https://pigamedrv.github.io',
+# Module
+py_modules=['pigame']
+)
